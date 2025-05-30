77efeea04bf5d8aa0a2da7bd4a4cd537adf52173
saraiGtz <sgutierrez@videsa.com.mx>
2025-05-30 12:38:18 -0600

    Inicio de creacion de plantillas de PDF

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -4,22 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Dompdf\Dompdf;
+
 //use PDF; 
 
 class PdfController extends Controller
 {
+    /* con esto ya descarga
     public function generatePdf()
 {
     return view('pdf.template');
+}*/
+
+
+
+public function generatePdf()
+{
+    $pdf = Pdf::loadView('pdf.template085MG');
+    return $pdf->download('plantilla.pdf');
 }
 
 
-/* con esto ya descarga
-public function generatePdf()
-{
-    $pdf = Pdf::loadView('pdf.template');
-    return $pdf->download('plantilla.pdf');
-}*/
 
 
 
