8a719863eb14ec5801d1f7ab039c81235e18e0e0
FreddieVen <fventura@videsa.com.mx>
2025-05-30 16:02:05 -0600

    Routes fixed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Vistas_Controller;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PdfController;
@@ -16,3 +17,11 @@ Route::get('/agregar_servicio', [Vistas_Controller::class, 'RegistrarServicio'])
 //RUTAS PARA PLANTILLAS PDF
 Route::get('generate085MG', [PdfController::class, 'generatePdf085MG']);
 Route::get('generate085G', [PdfController::class, 'generatePdf085G']);
+
+#--- Ruta Administrador ---#
+Route::get('/search',[Vistas_Controller::class, 'Buscar'])->name('Buscar');
+Route::get('/servicios_registrados', [Vistas_Controller::class, 'VistaServiciosRegistrados'])->name('ServiciosRegistrados');
+
+#--- Login & LogOut--#
+Route::post('/login', [LoginController::class, 'LogIn'])->name('Log_In');
+Route::post('/logout', [LoginController::class, 'LogOut'])->name('LogOut');
