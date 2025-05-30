16740e3921d816e68690b5a25d7c3609db7c0c12
FreddieVen <fventura@videsa.com.mx>
2025-05-30 15:45:02 -0600

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Vistas_Controller;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PdfController;
 
 #--- Ruta Login ---#
 Route::get('/', [Vistas_Controller::class, 'Login'])->name('login');
@@ -11,9 +11,8 @@ Route::get('/', [Vistas_Controller::class, 'Login'])->name('login');
 Route::get('/home', [Vistas_Controller::class, 'Home'])->name('Home');
 Route::get('/registrar_cliente', [Vistas_Controller::class, 'RegistrarCliente'])->name('RegistrarCliente');
 Route::get('/agregar_servicio', [Vistas_Controller::class, 'RegistrarServicio'])->name('AgregarServicio');
-Route::get('/search',[Vistas_Controller::class, 'Buscar'])->name('Buscar');
-Route::get('/servicios_registrados', [Vistas_Controller::class, 'VistaServiciosRegistrados'])->name('ServiciosRegistrados');
 
-#--- Login & LogOut--#
-Route::post('/login', [LoginController::class, 'LogIn'])->name('Log_In');
-Route::post('/logout', [LoginController::class, 'LogOut'])->name('LogOut');
+
+//RUTAS PARA PLANTILLAS PDF
+Route::get('generate085MG', [PdfController::class, 'generatePdf085MG']);
+Route::get('generate085G', [PdfController::class, 'generatePdf085G']);
