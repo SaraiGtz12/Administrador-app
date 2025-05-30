b394e2bcab99689d8b943db9d4b008917abd2ff8
saraiGtz <sgutierrez@videsa.com.mx>
2025-05-30 08:47:51 -0600

    Inicio de creacion de plantillas de PDF

---
+++ app/Http/Controllers/PdfController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
+//use PDF; 
+
+class PdfController extends Controller
+{/*
+    public function generatePdf()
+{
+    return view('pdf.template');
+}*/
+
+
+public function generatePdf()
+{
+    $pdf = Pdf::loadView('pdf.template');
+    return $pdf->download('plantilla.pdf');
+}
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Vistas_Controller;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PdfController;
 
 #--- Ruta Login ---#
 Route::get('/', [Vistas_Controller::class, 'Login'])->name('login');
@@ -10,3 +11,7 @@ Route::get('/', [Vistas_Controller::class, 'Login'])->name('login');
 Route::get('/home', [Vistas_Controller::class, 'Home'])->name('Home');
 Route::get('/registrar_cliente', [Vistas_Controller::class, 'RegistrarCliente'])->name('RegistrarCliente');
 Route::get('/agregar_servicio', [Vistas_Controller::class, 'RegistrarServicio'])->name('AgregarServicio');
+
+
+//RUTAS PARA PLANTILLAS PDF
+Route::get('generate-pdf', [PdfController::class, 'generatePdf']);
